5e6027360c4ce83b15877ba7f4a09a14f0dfe541
Inflex <pldaniels@gmail.com>
2018-06-24 17:44:41 +1000

    Renaming of resulting mupdf file to OS specific

--- build.sh
+++ build.sh
@@ -2,3 +2,10 @@
 export G=`git rev-list HEAD --count`
 echo $G
 make -f Makefile.general GIT_BUILD='${G}' $1
+if [ $? -eq 0 ]; then
+	if [ ${OS_TYPE} == "linux" ]; then
+		mv build/release/mupdf-gl build/release/mupdf-linux
+	elif [ ${OS_TYPE} == "macos" ]; then
+		mv build/release/mupdf-gl build/release/mupdf-macos
+	fi
+fi
