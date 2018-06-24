d097342237a4d4526e268d8fddef00ea165cf98d
Inflex <pldaniels@gmail.com>
2018-06-24 17:54:21 +1000

    More travis/build

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 export G=`git rev-list HEAD --count`
 echo $G
-make -f Makefile.general GIT_BUILD='${G}' $1
+make -f Makefile.general GIT_BUILD='${G}' 
 if [ $? -eq 0 ]; then
-	if [ ${OS_TYPE} == "linux" ]; then
+	if [ $1 == "linux" ]; then
 		mv build/release/mupdf-gl build/release/mupdf-linux
-	elif [ ${OS_TYPE} == "macos" ]; then
+	elif [ $1 == "osx" ]; then
 		mv build/release/mupdf-gl build/release/mupdf-macos
 	fi
 fi
