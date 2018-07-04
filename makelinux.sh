4ee3b16e347d89a8897e342e5549741c7dff49b4
Inflex <pldaniels@gmail.com>
2018-07-04 15:33:26 +1000

    Get pedantic with build scripts

---
+++ makelinux.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -x
+
+export G=`git rev-list HEAD --count`
+echo $G
+make -f Makefile.general GIT_BUILD='${G}'  $1
+MR=$?
+if [ $MR -eq 0 ]; then
+	mv build/release/mupdf-gl build/release/mupdf-linux
+fi
+exit $MR
