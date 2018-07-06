14a639c6a342e5c5f3d222c598d6bb5b06f091d2
Paul Daniels <pld@Pauls-MacBook-Pro.local>
2018-07-06 18:13:20 +1000

    make-bundle updated to create dmg install package

--- make-bundle.sh
+++ make-bundle.sh
@@ -6,12 +6,15 @@ set -x
 D=fbvpdf.app
 PL=$D/Info.plist
 mkdir $D
+mkdir tmp
 mkdir $D/Contents
 mkdir $D/Contents/MacOS
 mkdir $D/Contents/Resources
 cp -rv resources/Frameworks $D/Contents/
 cp -v Info.plist $D/Contents
-cp -v build/flexbv-macos $D/Contents/MacOS/fbvpdf
-cp -v fbvpdfIcon.png $D/Contents/Resources/fbvpdf.icns
+cp -v build/release/mupdf-macos $D/Contents/MacOS/fbvpdf
+cp -v icon-128.png $D/Contents/Resources/fbvpdf.icns
 
 
+hdiutil create tmp/tmp.dmg -ov -volname "FlexBV PDF Viewer" -fs HFS+ -srcfolder fbvpdf.app
+hdiutil convert tmp/tmp.dmg -format UDZO -o fbvpdfInstall.dmg
