386ce08a11162497660a6ace0565285a7c8d1417
Inflex <pldaniels@gmail.com>
2018-08-07 19:46:43 +1000

    Minor updates

--- platform/gl/gl-main.c
+++ platform/gl/gl-main.c
@@ -69,33 +69,6 @@ static GLint max_texture_size = 8192;
 #ifdef __WIN32
 
 #include    <windows.h>
-
-DWORD getppid() {
-    HANDLE hSnapshot;
-    PROCESSENTRY32 pe32;
-    DWORD ppid = 0, pid = GetCurrentProcessId();
-
-    hSnapshot = CreateToolhelp32Snapshot( TH32CS_SNAPPROCESS, 0 );
-    __try{
-        if( hSnapshot == INVALID_HANDLE_VALUE ) __leave;
-
-        ZeroMemory( &pe32, sizeof( pe32 ) );
-        pe32.dwSize = sizeof( pe32 );
-        if( !Process32First( hSnapshot, &pe32 ) ) __leave;
-
-        do{
-            if( pe32.th32ProcessID == pid ){
-                ppid = pe32.th32ParentProcessID;
-                break;
-            }
-        }while( Process32Next( hSnapshot, &pe32 ) );
-
-    }
-    __finally{
-        if( hSnapshot != INVALID_HANDLE_VALUE ) CloseHandle( hSnapshot );
-    }
-    return ppid;
-}
 #else
 
 #endif
